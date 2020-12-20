97381c6233cbea2eae3ebcefca20e1ba47fb1c6b
PawelZapala <pawelzapala007@gmail.com>
2020-12-20 17:43:13 +0100

    second commit

--- Zaliczenie/script.js
+++ Zaliczenie/script.js
@@ -1,13 +1,17 @@
 $(function () {
 
-    var nameB = 0;
-
+    var nameB = $('.name');
     var button = $('.add');
     var box2 = $('#box2')
+    var buy = $('#buy');
+    var basket = $('.basket');
+
+    box2 = 0;
 
     $('.add').on('click', function () {
         document.getElementById('box2').textContent = ++box2;
     });
+
 
     var box1 = 0;
 
@@ -16,5 +20,17 @@ $(function () {
         var newElement = $('#box2');
         basket.append(newElement);
     });
+
+    basket.on('click', '#delete', function (){
+        $(box2).remove();
+    });
+
+    basket.on('click', '#remove', function (){
+        $(box1).remove();
+    });
+
+     $('#buy').on('click', function () {
+            windows.alert('Dokonujesz zakupu' + name + '. Cena: ' + price);
+        });
 
 });
