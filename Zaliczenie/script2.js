9d9b6e76750a4c86e0f5c5a0a8c8eaab12c9892e
PawelZapala <pawelzapala007@gmail.com>
2021-01-19 17:48:24 +0100

    add button buy, remove, clearAll and window alert

--- Zaliczenie/script2.js
+++ Zaliczenie/script2.js
@@ -1,31 +1,36 @@
  $(function () {
 
      var buttonAdd = $('.add');
-     var buttonDelete = $('#delete');
-     var buttonRemove = $('#remove');
+     var buttonRemoveAll = $('#removeAll');
      var box2 = $('#box2');
      var buttonBuy = $('#buy');
      var basket = $('.basket');
      var name = $('.name');
-     var price = $('.price');
-     var basketPrice = $('.basketPrice')
-     /*var delete = $('#delete');*/
+     var price = parseInt($('.price').attr('id'),10);
+     var basketPrice = $('.basketPrice');
+     var buttonDelete = $('.delete');
 
     basketPrice = 0;
 
-+    buttonAdd.on('click', function () {
-         basketPrice = price++;
-         var newElement = $('<div id="box2">' + $(this).next().text() + $(this).parents('.box3').children('.name').text() + '</div>');
+    buttonAdd.on('click', function () {
+         var newElement = $('<div class="box2">' + $(this).parents('.box3').children('.name').text() + " " + '<button class="delete">Usuń</button>' + '<p>' + $(this).next().text() + '</div>');
          basket.append(newElement);
      });
 
-+    buttonRemove.on('click', '#box2', function () {
-         $(this).remove();
-     });
+    $('body').on('click', '.delete', function () {
+        $(this).closest('div').remove();
+    });
+
+    buttonBuy.on('click', function () {
+        window.alert('Dokonałeś zakupu w kwocie ' + basketPrice + " zł.");
+    });
+
+    $('body').on('click', '#removeAll', function () {
+        $('.box2').remove();
+    });
+
+    $('.basket').ready(function() {
+        newElement.attr('maxlength','8');
+    });
 
  });
-
-  function remove(id) {
-  	console.log(id);
-  	$("#"+id).remove();
-  };
